a998ec2419598229452ca204fd4fea724eaad2c7
Oleg Paramonov <68478325+oregu1@users.noreply.github.com>
2023-01-17 23:21:07 +0700

    Player movement and sprite library assets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float playerSpeed = 7.0f;
 
+    float movement = 0;
+
+    //REFERENCES
+    private Rigidbody2D playerRB;
+    private Animator playerAnimator;
+
+    private enum AnimationState
+    {
+        idle,
+        run,
+        fall,
+        jump,
+        dblJump,
+        hit
+    }
 
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+
+        playerRB.velocity = new Vector2(movement * playerSpeed, playerRB.velocity.y);
     }
 }
